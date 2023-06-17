3ffafafd4c9c3bd687552dd0a2159d861d25da79
Mikiya Mizui <mikiya.mizui@gmail.com>
2023-06-18 03:32:33 +0900

    add App.stories.js

---
+++ src/App.stories.js
@@ -0,0 +1,10 @@
+import App from "./App";
+
+/** @type {import("@storybook/react").Meta} */
+export default {
+  component: App,
+  args: {},
+};
+
+/** @type {import("@storybook/react").StoryObj} */
+export const Normal = {};
